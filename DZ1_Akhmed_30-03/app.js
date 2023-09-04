87635598225a1a11bbbe008803d9894dd1c2fe8c
sartovAkhmed <143886309+sartovAkhmed@users.noreply.github.com>
2023-09-04 23:44:40 +0600

    Update app.js

--- DZ1_Akhmed_30-03/app.js
+++ DZ1_Akhmed_30-03/app.js
@@ -1,34 +1,33 @@
 var month = parseInt(prompt('Введите месяц от 1-12'))
 var day = parseInt(prompt('Введите день рождение 1-31'))
-var result
 
-if ((month === 3 && day >= 21) || (month === 4 && day <= 20)) {
-    result = 'Овен'
-}else if ((month === 4 && day >= 21) || (month === 5 && day <= 21)) {
-    result = 'Телец'
-}else if ((month === 5 && day >= 22) || (month === 6 && day <= 21)) {
-    result = 'Близнецы'
-}else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-    result = 'Рак'
-}else if ((month === 7 && day >= 23) || (month === 8 && day <= 23)) {
-    result = 'Лев'
-}else if ((month === 8 && day >= 24) || (month === 9 && day <= 22)) {
-    result = 'Дева'
-}else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
-    result = 'Весы'
-}else if ((month === 10 && day >= 23) || (month === 11 && day <= 22)) {
-    result = 'Скорпион'
-}else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-    result = 'Стрелец'
-}else if ((month === 12 && day >= 22) || (month === 1 && day <= 20)) {
-    result = 'Козерог'
-}else if ((month === 1 && day >= 21) || (month === 2 && day <= 19)) {
-    result = 'Водолей'
-}else if ((month === 2 && day >= 20) || (month === 3 && day <= 20)) {
-    result = 'Рыбы'
+if ((month <= 12 && day <= 31) && (month >= 1 && day >= 1)) {
+    if ((month === 3 && day >= 21) || (month === 4 && day <= 20)) {
+        alert('Ваш знак зодиака: Овен')
+    }else if ((month === 4 && day >= 21) || (month === 5 && day <= 20)) {
+        alert('Ваш знак зодиака: Телец')
+    }else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
+        alert('Ваш знак зодиака: Близнецы')
+    }else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
+        alert('Ваш знак зодиака: Рак')
+    }else if ((month === 7 && day >= 23) || (month === 8 && day <= 23)) {
+        alert('Ваш знак зодиака: Лев')
+    }else if ((month === 8 && day >= 24) || (month === 9 && day <= 22)) {
+        alert('Ваш знак зодиака: Дева')
+    }else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
+        alert('Ваш знак зодиака: Весы')
+    }else if ((month === 10 && day >= 24) || (month === 11 && day <= 22)) {
+        alert('Ваш знак зодиака: Скорпион')
+    }else if ((month === 11 && day >= 23) || (month === 12 && day <= 21)) {
+        alert('Ваш знак зодиака: Стрелец')
+    }else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+        alert('Ваш знак зодиака: Козерог')
+    }else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+        alert('Ваш знак зодиака: Водолей')
+    }else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+        alert('Ваш знак зодиака: Рыбы')
+    }
 }else {
     alert('ERROR')
     console.error('ERROR')
 }
-
-alert(`Ваш знак зодиака: ${result}`)
